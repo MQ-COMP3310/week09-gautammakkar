1f0c448ca9feb3ae63683fb0f58c8513baebe7e9
gautammakkar <gautam.makkar@students.mq.edu.au>
2025-05-08 10:38:59 +1000

    commit2

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from project import create_app
-
+import os
 if __name__ == '__main__':
   app = create_app()
-  app.run(host = '127.0.0.1', port = 8001, debug=True) 
+  debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 't']
+  app.run(host='127.0.0.1', port=8001, debug=debug_mode)
